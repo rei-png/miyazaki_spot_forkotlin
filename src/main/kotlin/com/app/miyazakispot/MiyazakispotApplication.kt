3619e3dc91ce50608dbb90ae06de9e5679311981
r-kurogi <kuro.rei.8334@icloud.com>
2024-05-11 20:18:44 +0900

    first commit

---
+++ src/main/kotlin/com/app/miyazakispot/MiyazakispotApplication.kt
@@ -0,0 +1,11 @@
+package com.app.miyazakispot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MiyazakispotApplication
+
+fun main(args: Array<String>) {
+	runApplication<MiyazakispotApplication>(*args)
+}
---
+++ src/test/kotlin/com/app/miyazakispot/MiyazakispotApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.app.miyazakispot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MiyazakispotApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
