78baed5e317c48eaeff75c6fcbe185036e4fbca5
r-kurogi <kuro.rei.8334@icloud.com>
2024-05-11 21:12:02 +0900

    初期セットアップ

--- src/main/kotlin/com/app/miyazakispot/MiyazakiSpotApplication.kt
+++ src/main/kotlin/com/app/miyazakispot/MiyazakiSpotApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class MiyazakispotApplication
+class MiyazakiSpotApplication
 
 fun main(args: Array<String>) {
-	runApplication<MiyazakispotApplication>(*args)
+	runApplication<MiyazakiSpotApplication>(*args)
 }
